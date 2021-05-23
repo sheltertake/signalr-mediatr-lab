4d5a98200fdf3df8d59f452952b3ff3348966575
Massimiliano Balestrieri <massimiliano.balestrieri@deltatre.it>
2021-05-23 18:32:01 +0200

    rover experiment

--- backend/src/BackendApi/Startup.cs
+++ backend/src/BackendApi/Startup.cs
@@ -25,6 +25,7 @@ namespace BackendApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IRoverService, RoverService>();
             services.AddSignalR();
             services.AddMediatR(typeof(Startup));
         }
@@ -61,14 +62,48 @@ namespace BackendApi
             });
         }
     }
+    
 
-    public interface INotificationHubClient
+    public class RoverState
     {
-        Task SendPongNotificationAsync(PongNotification notification);
-        Task SendPingNotificationAsync(string data);
+        public string Name = "Foo";
+        //public bool?[,] Map { get; private set; } = new bool?[3, 3];
+        //public RoverState()
+        //{
+        //    Map[0, 0] = false;
+        //    Map[2, 2] = false;
+        //    Map[0, 2] = false;
+        //    Map[2, 0] = false;
+        //    Map[1, 1] = true;
+        //}
+
     }
 
-    public class NotificationHub : Hub<INotificationHubClient>
+    public interface IRoverService
+    {
+        Task<RoverState> LandAsync();
+    }
+    public class RoverService : IRoverService
+    {
+        public Task<RoverState> LandAsync()
+        {
+            return Task.FromResult(new RoverState());
+        }
+    }
+    public interface IPushNotificationHubClient
+    {
+        Task SendPongNotificationAsync(PongNotification notification);
+        Task RoverLandResponseAsync(string state);
+    }
+
+    public interface IInvokeNotificationHubClient
+    {
+        Task SendPingNotificationAsync(string data);
+        Task RoverLandRequestAsync();
+    }
+
+
+    public class NotificationHub : Hub<IPushNotificationHubClient>, IInvokeNotificationHubClient
     {
         private readonly IMediator _mediator;
         private readonly ILogger<NotificationHub> _logger;
@@ -88,16 +123,75 @@ namespace BackendApi
 
             return _mediator.Publish(new PingNotification(Context.ConnectionId, data));
         }
+
+        public Task RoverLandRequestAsync()
+        {
+            return _mediator.Publish(new RoverLandRequest(Context.ConnectionId));
+        }
     }
-    public class PingNotification : INotification
+
+    public class RoverLandRequest : INotification
     {
         public string ConnectionId { get; }
-        public string Data { get; }
-
-        public PingNotification(string connectionId, string data)
+        public RoverLandRequest(string connectionId)
         {
             ConnectionId = connectionId;
-            Data = data;
+        }
+    }
+    public class RoverLandRequestNotificationHandler : INotificationHandler<RoverLandRequest>
+    {
+        private readonly IRoverService _roverService;
+        private readonly IMediator _mediator;
+        private readonly ILogger<RoverLandRequestNotificationHandler> _logger;
+
+        public RoverLandRequestNotificationHandler(
+            IRoverService roverService,
+            IMediator mediator,
+            ILogger<RoverLandRequestNotificationHandler> logger)
+        {
+            _roverService = roverService;
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        public Task Handle(RoverLandRequest notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("RoverLandRequestNotificationHandler - {notificationConnectionId}", notification.ConnectionId);
+            return _roverService.LandAsync().ContinueWith(x =>
+            {
+                _logger.LogInformation("LandAsync - "+  x.Result.Name);
+                _mediator.Publish(new RoverLandResponse(notification.ConnectionId, x.Result), cancellationToken);
+            }, cancellationToken);
+        }
+    }
+    public class RoverLandResponseNotificationHandler : INotificationHandler<RoverLandResponse>
+    {
+        private readonly IHubContext<NotificationHub, IPushNotificationHubClient> _hubContext;
+        private readonly ILogger<RoverLandResponseNotificationHandler> _logger;
+
+        public RoverLandResponseNotificationHandler(
+            IHubContext<NotificationHub, IPushNotificationHubClient> hubContext,
+            ILogger<RoverLandResponseNotificationHandler> logger)
+        {
+            _hubContext = hubContext;
+            _logger = logger;
+        }
+
+        public Task Handle(RoverLandResponse notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("RoverLandResponseNotificationHandler - {notificationConnectionId}", notification.ConnectionId);
+            return _hubContext.Clients.Clients(notification.ConnectionId).RoverLandResponseAsync(notification.State.Name);
+        }
+    }
+    public class RoverLandResponse : INotification
+    {
+        public string ConnectionId { get; }
+        public RoverState State { get; }
+
+        public RoverLandResponse(string connectionId, RoverState state)
+        {
+            ConnectionId = connectionId;
+            State = state;
         }
     }
     public class PingNotificationHandler : INotificationHandler<PingNotification>
@@ -123,11 +217,11 @@ namespace BackendApi
     }
     public class PongNotificationHandler : INotificationHandler<PongNotification>
     {
-        private readonly IHubContext<NotificationHub, INotificationHubClient> _hubContext;
+        private readonly IHubContext<NotificationHub, IPushNotificationHubClient> _hubContext;
         private readonly ILogger<PongNotificationHandler> _logger;
 
         public PongNotificationHandler(
-            IHubContext<NotificationHub, INotificationHubClient> hubContext,
+            IHubContext<NotificationHub, IPushNotificationHubClient> hubContext,
             ILogger<PongNotificationHandler> logger)
         {
             _hubContext = hubContext;
@@ -151,6 +245,17 @@ namespace BackendApi
         public string Data { get; }
 
         public PongNotification(string connectionId, string data)
+        {
+            ConnectionId = connectionId;
+            Data = data;
+        }
+    }
+    public class PingNotification : INotification
+    {
+        public string ConnectionId { get; }
+        public string Data { get; }
+
+        public PingNotification(string connectionId, string data)
         {
             ConnectionId = connectionId;
             Data = data;
--- frontend/src/app/services/signal-r.service.ts
+++ frontend/src/app/services/signal-r.service.ts
@@ -16,21 +16,30 @@ export class SignalRService {
       .start()
       .then(() => 
       {
-        console.log('Connection started');
-        console.log("Invoke SendPingNotificationAsync", this.counter)
-        this.hubConnection.invoke('SendPingNotificationAsync', '1 Frontend send message');
+        // console.log('Connection started');
+        // console.log("Invoke SendPingNotificationAsync", this.counter)
+        // this.hubConnection.invoke('SendPingNotificationAsync', '1 Frontend send message');
+
+        console.log('Invoke RoverLandRequestAsync');
+        this.hubConnection.invoke('RoverLandRequestAsync');
       })
       .catch(err => console.log('Error while starting connection: ' + err))
   }
   public registerOnServerEvents(){
+    // this.hubConnection.on(
+    //   'SendPongNotificationAsync',
+    //   (data: any) => {
+    //       console.log('Listening SendPongNotificationAsync - received', data, ++this.counter);
+    //       setTimeout(() =>{
+    //         console.log("Invoke SendPingNotificationAsync", this.counter)
+    //         this.hubConnection.invoke('SendPingNotificationAsync',  this.counter + ' Frontend send message');
+    //       }, 1000);          
+    //   });
+
     this.hubConnection.on(
-      'SendPongNotificationAsync',
+      'RoverLandResponseAsync',
       (data: any) => {
-          console.log('Listening SendPongNotificationAsync - received', data, ++this.counter);
-          setTimeout(() =>{
-            console.log("Invoke SendPingNotificationAsync", this.counter)
-            this.hubConnection.invoke('SendPingNotificationAsync',  this.counter + ' Frontend send message');
-          }, 1000);          
+          console.log('Listening RoverLandResponseAsync - received', data);
       });
   }
 }
